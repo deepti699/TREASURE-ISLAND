956e10ecef2edca3fe79bd7317c3492d2c2d236a
DEEPTI BS <deeptibalgar@gmail.com>
2026-02-24 21:14:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
+print("WELCOME TO TREASURE ISLAND")
 
+print(" YOUR MISSION IS TO FIND THE TREASURE")
+
+print("YOUR AT CROSS ROAD,WHERE DO YOU WANT TO GET?")
+
+choice = input("left or right")
+
+if choice == "left":
+
+    print("swim or wait")
+
+    choice2 = input()
+
+    if choice2 == "wait":
+
+      print("which door do you want to enter??")
+
+      choice3 = input("blue, red ,yellow")
+
+      if choice3 == "blue":
+
+        print("GAME OVER!!")
+
+      elif choice3 == "red":
+
+        print("BURNED BY FIRE")
+
+      elif choice3 == "yellow":
+
+        print("YOU WIN!!!")
+
+      else:
+
+        print(""GAME OVER")
+
+   else:
+
+       print("GAME OVER")
+
+else:
+
+  print("GAME OVER")
+
+    
+        
+
+        
